bd2aa6d390bb3a920c0e4e04c07707b0b88ccf8f
Leabs <stevenleabo@gmail.com>
2023-01-21 22:19:29 -0500

    Adding styles, screnshot, word output

--- MMM-urbandictionary.js
+++ MMM-urbandictionary.js
@@ -1,26 +1,81 @@
-//helloworld.js:
-
+/* MagicMirror²
+ * Module: MMM-urbandictionary
+ *
+ * By Steven Leabo https://stevenleabo.com
+ * MIT Licensed.
+ */
 Module.register("MMM-urbandictionary", {
-  // Default module config.
+  // Module config defaults.
   defaults: {
-    text: "Hello World!",
-  },
-
-  getScripts: function () {
-    return [
-      "script.js", // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-      "moment.js", // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-      this.file("anotherfile.js"), // this file will be loaded straight from the module folder.
-      "https://code.jquery.com/jquery-2.2.3.min.js", // this file will be loaded from the jquery servers.
-    ];
+    //update daily
+    updateInterval: 86400000,
+    //fade speed
+    fadeSpeed: 4000,
+    //initial load delay
+    initialLoadDelay: 0,
+    //retry delay
+    retryDelay: 2500,
+    //api url
+    apiBase: "https://api.urbandictionary.com/v0/random"
   },
   getHeader: function () {
-    return "Urbandictionary Word";
+    return "Urban Dictionary Random Word";
   },
+  getStyles: function () {
+    return [
+      "style.css" // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+    ];
+  },
+
+  // Define start sequence.
+  start: function () {
+    Log.info("Starting module: " + this.name);
+
+    this.lastComplimentIndex = -1;
+    this.word = "";
+    // Schedule update timer.
+    setInterval(() => {
+      this.updateDom(this.config.fadeSpeed);
+    }, this.config.updateInterval);
+  },
+
+  /**
+   * Retrieve the word of the day along with description, example, and author.
+   *
+   * @returns {string[]} array with word, description, example, and author
+   */
+  getWord: async function () {
+    const response = await fetch(this.config.apiBase);
+    const json = await response.json();
+    const word = json.list[0].word;
+    const definition = json.list[0].definition;
+    const example = json.list[0].example;
+    const author = json.list[0].author;
+    return [word, definition, example, author];
+  },
+
   // Override dom generator.
   getDom: function () {
-    var wrapper = document.createElement("div");
-    wrapper.innerHTML = "word here";
+    const wrapper = document.createElement("div");
+    wrapper.className = this.config.classes
+      ? this.config.classes
+      : "urban bright pre-line";
+    //wrapper.innerHTML = this.config.word;
+    this.getWord().then((response) => {
+      wrapper.innerHTML =
+        "<div class='urban-word'>" +
+        response[0] +
+        "</div>" +
+        "Description: " +
+        response[1] +
+        "<br>" +
+        "Example: " +
+        response[2] +
+        "<br>" +
+        "Author: " +
+        response[3];
+    });
+    //wrapper.innerHTML = this.config.word;
     return wrapper;
-  },
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MMM-UrbanDictionary
+
 Magic Mirror Module to show random daily word from urbandictionary.com
+
+![MMM-urbandictionary module screenshot](/screenshot/mirror.png)
+
+## Installation
+
+1. Navigate into your MagicMirror's `modules` folder and execute `git clone
